d8c5fd246e808f5a9a7c1295d6e0a780f85cb788
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-09-06 21:43:55 +0200

    controllo array

--- Js/script.js
+++ Js/script.js
@@ -5,20 +5,19 @@
 4. Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
 
+
 // 1. Un alert() espone 5 numeri generati casualmente => ciclo for * 5 (math.random) all'interno di un alert => salvare la lista di numeri in un array
 var array1 = [];
 
 for (var i = 0; i<5; i++) {
-    var Num = Math.floor(Math.random() * 100)+1;
+    var Num = Math.floor(Math.random() * 10)+1;
     array1.push(Num); 
     alert(Num);
 }
-console.log(array1);
 
 
 // 2. Da li parte un timer di 30 secondi 
-var secondi = 3;
-
+var secondi = 5;
 var array2 = []
 
 var timer = setInterval(function() {
@@ -27,12 +26,19 @@ var timer = setInterval(function() {
     secondi--;
 
     if ( secondi == -1 ) {
+
+        document.getElementById("Timer").innerHTML = "";
         clearInterval(timer);
+
 
         // 3. Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt()
         for (var i = 0; i<5; i++) {
         var number = parseInt(prompt("Inserisci il numero corrispondente"));
-        console.log(number);
+        // controllo inserimento dato
+        while ( isNaN(number) ) {
+            alert("Error");
+            number = parseInt(prompt("Inserisci il numero corrispondente"));
+        }
         array2.push(number); 
         }
 
@@ -40,30 +46,30 @@ var timer = setInterval(function() {
         console.log(array2);
 
 
+        // 4. Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
+        document.getElementById("Risultato").innerHTML = ``;
+        var arrayFiltered = [];
 
-        if ( array1 == array2 ) {
-            alert("Bravo!");
+        // controllo singoli elementi dei due array
+        for ( i=0; i<5; i++ ) {
+
+            if ( array1[i] == array2[i] ) {
+                document.getElementById("Risultato").innerHTML += `Corretto! Il numero da te inserito è ${array2[i]} <br>`;
+                arrayFiltered.push(array2[i])
+                
+            } else {
+                document.getElementById("Risultato").innerHTML += `Errato! Il numero da te inserito è ${array2[i]} <br>`;
+            }
+        }
+
+        // controllo finale
+        if ( arrayFiltered.length == 5 ) {
+            document.getElementById("Risultato").innerHTML += `Bravo! Li hai indovinati tutti!`;
+        } else if (arrayFiltered.length == 0 ) {
+            document.getElementById("Risultato").innerHTML += `Mi dispiace, non ne hai indovinato nemmeno uno!`;
+        } else {
+            document.getElementById("Risultato").innerHTML += `Hai indovinato ${arrayFiltered.length} numeri/o`;
         }
     }
 
 },1000);
-
-
-
-
-
-// // 3. Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt()
-// for (var i = 0; i<5; i++) {
-//     var number = parseInt(prompt("Inserisci il numero corrispondente"));
-//     console.log(number);
-//     array2.push(number); 
-//     // alert(number);
-// }
-
-// // 4. Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
-// // (switch => per confrontare singolarmente i valori dei due array)
-// if ( array1 == array2 ) {
-//     alert("Bravo!");
-// } else {
-//     alert("no...");
-// }
